804930cfa947d014989ac0ed173abb7ec2414bd4
Linnea Larsson <philar-9@student.ltu.se>
2024-11-21 15:44:00 +0100

    Nu med asciidraken.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //test
+        System.out.println("Hello world!");
     }
-}
+
+    private void test() {
+
+    }
+}//main

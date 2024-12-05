ba3655aeecdefe54c131835e543b0aac2df7c69d
Zekas-chan <philar-9@student.ltu.se>
2024-12-05 12:10:58 +0100

    Implementerade door, ett antal användbara metoder och konstruktorn: getCardinalDirection, ger till exempel north istället för n getPosition, ger riktningen som char, till exempel n konstruktorn, kräver en char.

--- src/Door.java
+++ src/Door.java
@@ -1,16 +1,53 @@
 public class Door {
     private boolean locked;
-    private char position; //north, south, east, west
+    private char orientation; //north, south, east, west
 
 
-
-
-    public Door()
+    /**
+     * Konstruktor för dörr.
+     * @param orientation Riktningen i vilken dörren pekar
+     */
+    public Door(char orientation)
     {
-        //TODO implement constructor
+        this.orientation = Character.toLowerCase(orientation); //ser till att det alltid är liten bokstav
     }
 
-    public boolean isLocked(){
+    /**
+     * Get-metod för locked
+     * @return Huruvida dörren är låst eller inte
+     */
+    public boolean isLocked()
+    {
         return locked;
+    }
+
+    /**
+     * Get-metod för orientation
+     * @return Dörrens "position" som en char
+     */
+    public char getPosition()
+    {
+        return orientation;
+    }
+
+    /**
+     *
+     * @return Dörrens riktning utskriven
+     */
+    public String getCardinalDirection()
+    {
+        switch(orientation)
+        {
+            case 'n':
+                return "north";
+            case 'e':
+                return "east";
+            case 's':
+                return "south";
+            case 'w':
+                return "west";
+            default:
+                return "This should never display";
+        }
     }
 }

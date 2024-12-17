25503d2c1665912d54aa36f3a64a577fb49c7f68
Zekas-chan <philar-9@student.ltu.se>
2024-12-17 16:57:57 +0100

    Flyttade level ut ur DragonTreasure
    Tanken var att göra klassen mer överskådlig och ha förmågan att hantera flera levels, just nu endast en jag kallar test samt level 1 (den vi haft tidigare).
    
    levelLoader.java
    Denna klass har en metod för varje nivå och metoden selectLevel tar en int och returnerar motsvarande level i form av en Room[][] array. För att placera monster osv görs det rimligtvis i motsvarande metod.

--- src/DragonTreasure.java
+++ src/DragonTreasure.java
@@ -38,71 +38,18 @@ public class DragonTreasure {
      */
     public Dungeon setupGame()
     {
-        /* 4x3 map
-
-        null    dead    monster     exit
-        Start   torch   null        null
-        null    key     potion      dragon/treasure
-         */
-		Door[] door_start = {new Door('e', false)}; //door(s) for the room
-		StartRoom start = new StartRoom("You see a cave entrance to the east.", door_start); //the room
-
-
-		Door[] door_torch =
-                {
-                    new Door('n', false),
-                    new Door('s', false)
-                };
-        Room torch = new Room("The door to the west has collapsed. You see a torch on the wall.", door_torch);
-
-        Door[] door_dead =
-                {
-                    new Door('s', false),
-                    new Door('e', false)
-                };
-        Room dead = new Room("You see a dead person in the corner.", door_dead);
-
-        Door[] door_monster =
-                {
-                    new Door('w', false),
-                    new Door('e', false),
-                    new Door('s', false)
-                };
-        Room monster = new Room("You smell a musky smell.", door_monster);
-
-        Door[] door_potion =
-                {
-                    new Door('n', false),
-                    new Door('w', false),
-                    new Door('e', true)
-                };
-        Room potion = new Room("You hear a low growling.", door_potion);
-
-        Door[] door_key =
-                {
-                    new Door('n', false),
-                    new Door('e', false)
-                };
-        Room key = new Room("You hear water nearby.", door_key);
-
-        Door[] door_treasure = {new Door('w', false)};
-        Room treasure = new Room("Wow, TREASURE!", door_treasure);
-
-        Door[] door_exit = {new Door('w', false)};
-        ExitRoom exit = new ExitRoom("Congratulations, you made it!", door_exit);
-
-        //assemble rooms into map
-        Room[][] map =
-                {
-                    {null, dead, monster, exit}, //array [0][n]
-                    {start, torch, null, null}, //array [1][n]
-                    {null, key, potion, treasure}, //array [2][n]
-                };
+        //local variables
+        Scanner input = new Scanner(System.in);//Creates a scanner and pauses the program to wait for user input
+        levelLoader loader = new levelLoader(); //skapar en level när den anropas
 
         //create player
         System.out.println("Name your hero: ");
-        Scanner input = new Scanner(System.in);//Creates a scanner and pauses the program to wait for user input
         Player player = new Player(input.nextLine());//Assigns "player" the name that was input
+
+        //select level
+        System.out.println("Select level(0, 1): ");
+        int level = input.nextInt();
+        Room[][] map = loader.selectLevel(level);
 
 		//return playable dungeon
         return new Dungeon(map, welcomeMessage, player);
---
+++ src/levelLoader.java
@@ -0,0 +1,122 @@
+import Rooms.*;
+
+/**
+ * Håller spelets alla rum samt en metod för att välja bland dem.
+ */
+public class levelLoader{
+
+	public Room[][] selectLevel (int level)
+	{
+		switch (level)
+		{
+			case 0:
+				return test();
+			case 1:
+				return Level_1();
+			default:
+				return test();
+		}
+	}
+
+	/**
+	 * Testlevel
+	 * @return En enklare level som bara går rakt i en riktning.
+	 */
+	private Room[][] test()
+	{
+		Door[] door_start = {new Door('w', false)};
+		Room start = new StartRoom("Test start", door_start);
+
+		Door[] test1_door =
+				{
+					new Door('w', false),
+					new Door('e', false)
+				};
+		Room test1 = new Room("Test room 1", test1_door);
+
+		Door[] test2_door =
+				{
+						new Door('w', false),
+						new Door('e', false)
+				};
+		Room test2 = new Room("Test room 2", test2_door);
+
+		Door[] test3_door =
+				{
+						new Door('w', false),
+						new Door('e', false)
+				};
+		Room test3 = new Room("Test room 3", test3_door);
+
+		Door[] door_exit = {new Door('e', false)};
+		Room exit = new ExitRoom("Test room exit", door_exit);
+
+		return new Room[][]{{exit, test1, test2, test3, start}};
+
+	};
+
+	/**
+	 * Level 1, från klassdiagrammet.
+	 * @return En array av rum som har dörrar.
+	 */
+	private Room[][] Level_1(){
+		/* 4x3 map
+
+        null    dead    monster     exit
+        Start   torch   null        null
+        null    key     potion      dragon/treasure
+         */
+		Door[] door_start = {new Door('e', false)}; //door(s) for the room
+		StartRoom start = new StartRoom("You see a cave entrance to the east.", door_start); //the room
+
+		Door[] door_torch =
+				{
+						new Door('n', false),
+						new Door('s', false)
+				};
+		Room torch = new Room("The door to the west has collapsed. You see a torch on the wall.", door_torch);
+
+		Door[] door_dead =
+				{
+						new Door('s', false),
+						new Door('e', false)
+				};
+		Room dead = new Room("You see a dead person in the corner.", door_dead);
+
+		Door[] door_monster =
+				{
+						new Door('w', false),
+						new Door('e', false),
+						new Door('s', false)
+				};
+		Room monster = new Room("You smell a musky smell.", door_monster);
+
+		Door[] door_potion =
+				{
+						new Door('n', false),
+						new Door('w', false),
+						new Door('e', true)
+				};
+		Room potion = new Room("You hear a low growling.", door_potion);
+
+		Door[] door_key =
+				{
+						new Door('n', false),
+						new Door('e', false)
+				};
+		Room key = new Room("You hear water nearby.", door_key);
+
+		Door[] door_treasure = {new Door('w', false)};
+		Room treasure = new Room("Wow, TREASURE!", door_treasure);
+
+		Door[] door_exit = {new Door('w', false)};
+		ExitRoom exit = new ExitRoom("Congratulations, you made it!", door_exit);
+
+		return new Room[][]
+				{
+					{null, dead, monster, exit}, //array [0][n]
+					{start, torch, null, null}, //array [1][n]
+					{null, key, potion, treasure}, //array [2][n]
+				};
+	}
+}

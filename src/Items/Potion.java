99f75f691b8bd721abe8fa15adfea7a6539edac9
Ellni-ellni <ellnch-4@student.ltu.se>
2025-01-08 11:56:27 +0100

    Update Potion.java
    vet inte vad jag håller på med

--- src/Items/Potion.java
+++ src/Items/Potion.java
@@ -1,5 +1,26 @@
 package Items;
 
-public class Potion extends Item{
-	private int Healing;
+public class Potion extends Item {
+    private int healingAmount;
+
+    public Potion(String name, String description, int healingAmount) {
+        super(itemName, description);
+        this.healingAmount = healingAmount;
+    }
+
+    public int getHealingAmount() {
+        return healingAmount;
+    }
+
+    @Override
+    public void use() {
+        // Implement the effect of using the potion, like healing the player.
+        System.out.println("You use the " + getName() + " and heal " + healingAmount + " health points.");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nHealing Amount: " + healingAmount;
+    }
 }
+

f0a2d8b24184614164203d7d3ef9fa6a66cd4ff6
Ellni-ellni <ellnch-4@student.ltu.se>
2025-01-08 19:05:33 +0100

    comments

--- src/Items/Key.java
+++ src/Items/Key.java
@@ -6,14 +6,14 @@ public class Key extends Item {
     public Key(String name, String description) {
         super(name, description);
        
-    }
+    } //constructor for creating a key
 
 
     @Override
     public void use()
     {
         System.out.println("You use the " + getName() + " to unlock the door.");
-    }
+    } //defines the effect of using a key
 
   
 }

7d0f979245e44a351321925aa72a9de2f5179f5d
Ellni-ellni <ellnch-4@student.ltu.se>
2025-01-08 11:58:59 +0100

    Update Treasure.java

--- src/Items/Treasure.java
+++ src/Items/Treasure.java
@@ -1,5 +1,23 @@
-package Items;
+public class Treasure extends Item {
+    private int goldValue;
 
-public class Treasure extends Item{
-	private int goldValue;
+    public Treasure(String name, String description, int value) {
+        super(name, description);
+        this.goldValue = goldValue;
+    }
+
+    public int getgoldValue() {
+        return goldValue;
+    }
+
+    @Override
+    public void use() {
+     
+        System.out.println("You collect the treasure worth " + goldValue + " gold coins.");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nValue: " + goldValue;
+    }
 }

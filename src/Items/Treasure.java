7741d8794d8769af658da7291ad740f474390941
Ellni-ellni <ellnch-4@student.ltu.se>
2025-01-08 19:00:54 +0100

    Comments

--- src/Items/Treasure.java
+++ src/Items/Treasure.java
@@ -1,12 +1,12 @@
 package Items;
 
 public class Treasure extends Item {
-    private int goldValue;
+    private int goldValue; //the value of the treasure
 
     public Treasure(String name, String description, int value) {
         super(name, description);
         this.goldValue = value;
-    }
+    } //Constructor for creating treasure
 
     public int getgoldValue() {
         return goldValue;
@@ -16,10 +16,10 @@ public class Treasure extends Item {
     public void use() {
      
         System.out.println("You collect the treasure worth " + goldValue + " gold coins.");
-    }
+    } //method that defines the effect of "using" (picking up) treasure items 
 
     @Override
     public String toString() {
         return super.toString() + "\nValue: " + goldValue;
-    }
+    } //enables the value of the treasure to be viewed
 }

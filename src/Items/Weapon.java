839d9f34a9ba193f3f8021883c45ee8d47ff6160
Ellni-ellni <ellnch-4@student.ltu.se>
2025-01-08 18:45:45 +0100

    Update Weapon.java
    Comments

--- src/Items/Weapon.java
+++ src/Items/Weapon.java
@@ -1,14 +1,14 @@
 package Items;
 
 public class Weapon extends Item {
-    private int increaseDamage;
-    private String weaponType;
+    private int increaseDamage; //The amount of damage the player gains from weilding the weapon
+    private String weaponType; //Type of weapon ex. sword, bow, mace etc.
 
     public Weapon(String name, String description, int damage, String weaponType) {
         super(name, description);
         this.increaseDamage = damage;
         this.weaponType = weaponType;
-    }
+    } //constructor for creating weapons
 
     public int getDamage() {
         return increaseDamage;
@@ -20,12 +20,12 @@ public class Weapon extends Item {
 
     @Override
     public void use() {
-       
+       //Method that defines the effect of using the weapon
         System.out.println("You wield the " + getName() + " and can now deal " + increaseDamage + " more damage.");
     }
 
     @Override
     public String toString() {
         return super.toString() + "\nDamage: " + increaseDamage + "\nWeapon Type: " + weaponType;
-    }
+    } //to enable more information about the weapon to be viewed
 }

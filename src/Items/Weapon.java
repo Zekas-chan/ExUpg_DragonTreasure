b11b13077755958b828e3aff3c3ea0deed659a9e
Ellni-ellni <ellnch-4@student.ltu.se>
2025-01-08 12:05:13 +0100

    Update Weapon.java

--- src/Items/Weapon.java
+++ src/Items/Weapon.java
@@ -1,5 +1,31 @@
 package Items;
 
-public class Weapon extends Item{
-	private int IncreaseDamage;
+public class Weapon extends Item {
+    private int increaseDamage;
+    private String weaponType;
+
+    public Weapon(String name, String description, int damage, String weaponType) {
+        super(name, description);
+        this.increaseDamage = damage;
+        this.weaponType = weaponType;
+    }
+
+    public int getDamage() {
+        return increaseDamage;
+    }
+
+    public String getWeaponType() {
+        return weaponType;
+    }
+
+    @Override
+    public void use() {
+       
+        System.out.println("You wield the " + getName() + " and can now deal " + increaseDamage + " damage.");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nDamage: " + increaeDamage + "\nWeapon Type: " + weaponType;
+    }
 }

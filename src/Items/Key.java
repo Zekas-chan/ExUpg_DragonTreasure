4bd1c25051c5f002785a2a7c9d3a8f566a94ede6
Ellni-ellni <ellnch-4@student.ltu.se>
2025-01-08 12:06:37 +0100

    Update Key.java

--- src/Items/Key.java
+++ src/Items/Key.java
@@ -1,4 +1,23 @@
-package Items;
-
 public class Key extends Item {
+    private String keyType;
+
+    public Key(String name, String description, String keyType) {
+        super(name, description);
+        this.keyType = keyType;
+    }
+
+    public String getKeyType() {
+        return keyType;
+    }
+
+    @Override
+    public void use() {
+        
+        System.out.println("You use the " + getName() + " to unlock a " + keyType + ".");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nKey Type: " + keyType;
+    }
 }
